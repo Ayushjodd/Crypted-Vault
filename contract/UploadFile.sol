0e1f8db3af31206f8facd02cc63f4dd155124e6c
Ayushjodd <ayushkalonia2016@gmail.com>
2024-08-03 13:47:07 +0530

    added the smart contract

---
+++ contract/UploadFile.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0 <0.9.0;
+
+contract Upload {
+    mapping(address => string[]) private userFiles;
+    modifier onlyOwnerAccess(address _user) {
+        require(msg.sender == _user, "only owner can call this function");
+        _;
+    }
+    function uploadFile(address _user, string memory _ipfsHash) external {
+        userFiles[_user].push(_ipfsHash);
+    }
+    function viewFiles(
+        address _user
+    ) external view onlyOwnerAccess(_user) returns (string[] memory) {
+        return userFiles[_user];
+    }
+}
