795e0a28bc11c14a54e9708afa9a681f9b25cf03
Ayushjodd <ayushkalonia2016@gmail.com>
2024-08-05 00:32:58 +0530

    added routes and more

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const { MONGODB_URL, PORT } = require("./config/serverConfig");
+const authentication = require("./routes/authentication");
+const authentication = require("./routes/authentication");
+const { connectDB } = require("./db/connectTODb");
 
-app.listen(3000, () => {
-  console.log("server is running on port http://localhost:3000");
-});
+app.use(cors());
+app.use(express.json());
+app.use("/api", authentication);
+app.use("/api", uploadImageRoute);
+
+async function serverStart() {
+  try {
+    await connectDB(MONGODB_URL);
+    console.log("database connected");
+    const PORT = process.env.PORT;
+    app.listen(PORT, () => {
+      console.log("server is running on port http://localhost:3000");
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+serverStart();
